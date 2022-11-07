fcd21ebceea424cd79021346d4c9c448c4fb869f
t11s <greenfeatherhelp@gmail.com>
2022-11-06 18:04:41 -0800

    At the repass, they servin' Popeyes chicken

--- src/MrMorale.sol
+++ src/MrMorale.sol
@@ -9,34 +9,27 @@ import "./BigStepper.sol";
 contract MrMorale {
     address public buyoooor;
 
-    mapping(ERC721 => mapping(uint256 => mapping(uint256 => address))) sellooors;
-
-    function list(ERC721 token, uint256 id, uint256 price) public {
-        address stepper = LibStepper.findStepper(token, id, price);
-
-        sellooors[token][id][price] = msg.sender;
-
-        token.transferFrom(msg.sender, stepper, id);
-    }
-
-    function buy(ERC721 token, uint256 id, uint256 price) public payable {
+    function buy(ERC721 token, uint256 id, uint256 price, address selloooor) public payable {
         require(msg.value >= price);
 
-        payable(sellooors[token][id][price]).transfer(msg.value);
+        payable(selloooor).transfer(msg.value);
 
         buyoooor = msg.sender;
-        new BigStepper{salt: keccak256(abi.encode(price))}(token, id);
+        new BigStepper{salt: keccak256(abi.encode(price, selloooor))}(
+            token,
+            id
+        );
         delete buyoooor;
 
         // gg no re.
     }
 
     function delist(ERC721 token, uint256 id, uint256 price) public {
-        address sellooor = sellooors[token][id][price];
-        require(msg.sender == sellooor, "not bussin");
-
         buyoooor = msg.sender;
-        new BigStepper{salt: keccak256(abi.encode(price))}(token, id);
+        new BigStepper{salt: keccak256(abi.encode(price, msg.sender))}(
+            token,
+            id
+        );
         delete buyoooor;
     }
 }
