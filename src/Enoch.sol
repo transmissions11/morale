14ff6111dbe7444777d6e288ca26a0c96ce1ed80
t11s <greenfeatherhelp@gmail.com>
2022-11-06 16:26:54 -0800

    My spirit's awakened, my brain is asleep

---
+++ src/Enoch.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "solmate/tokens/ERC721.sol";
+
+// i couldnt sleep last night because i felt this shit
+
+contract Enoch {
+    function list(ERC721 token, uint256 id) {}
+}
