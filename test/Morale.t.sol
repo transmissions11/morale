fc750db25c3e72acb461864077623f8281d215e1
frankie <88221686+FrankieIsLost@users.noreply.github.com>
2022-11-06 20:55:18 -0800

    Every thought is creative, sometimes I'm afraid of my open mind (#2)

---
+++ test/Morale.t.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "forge-std/Test.sol";
+
+import {MockBlacklistERC721} from "./utils/MockBlacklistERC721.sol";
+
+import {Enoch} from "../src/Enoch.sol";
+import {MrMorale} from "../src/MrMorale.sol";
+
+contract MrMoraleTest is Test {
+    Enoch enoch;
+    MrMorale morale;
+
+    MockBlacklistERC721 erc721A;
+    MockBlacklistERC721 erc721B;
+
+    address addr1 = address(0x600DDAD);
+    address addr2 = address(0xBADDAD);
+
+    function setUp() public {
+        enoch = new Enoch();
+        morale = enoch.summonMorale();
+
+        erc721A = new MockBlacklistERC721("A", "A");
+        erc721A = new MockBlacklistERC721("B", "B");
+
+        erc721A.mint(addr1, 1);
+
+        vm.deal(addr1, 100 ether);
+        vm.deal(addr2, 100 ether);
+    }
+
+    function testPurchase() public {
+        uint256 price = 100;
+        uint256 id = 1;
+
+        address stepper = enoch.findStepper(morale, erc721A, id, price, addr1);
+
+        vm.prank(addr1);
+        erc721A.transferFrom(addr1, stepper, id);
+        assertEq(erc721A.ownerOf(id), stepper);
+
+        uint256 balanceBefore = addr1.balance;
+
+        vm.prank(addr2);
+        morale.buy{value: price}(erc721A, id, price, addr1);
+
+        uint256 balanceAfter = addr1.balance;
+
+        assertEq(balanceBefore, balanceAfter - price);
+
+        assertEq(erc721A.ownerOf(id), addr2);
+    }
+}
