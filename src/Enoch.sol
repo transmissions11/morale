b81651cba5239ecca5f2c894772391cf1df7e593
t11s <greenfeatherhelp@gmail.com>
2022-11-06 16:50:57 -0800

    Past life regressions to know my conditions

--- src/Enoch.sol
+++ src/Enoch.sol
@@ -10,12 +10,28 @@ import "./BigStepper.sol";
 contract Enoch {
     address public buyoooor;
 
+    mapping(ERC721 => mapping(uint256 => mapping(uint256 => address))) sellooors;
+
+    function delist(
+        ERC721 token,
+        uint256 id,
+        uint256 price
+    ) public {
+        address sellooor = sellooors[token][id][price];
+        require(msg.sender == sellooor, "not bussin");
+
+        buyoooor = msg.sender;
+        new BigStepper{salt: keccak256(abi.encode(price))}(token, id);
+        delete buyoooor;
+    }
+
     function list(
         ERC721 token,
         uint256 id,
         uint256 price
     ) public {
         address stepper = LibStepper.findStepper(token, id, price);
+        sellooors[token][id][price] = msg.sender;
         token.transferFrom(msg.sender, stepper, id);
     }
 
@@ -25,6 +41,8 @@ contract Enoch {
         uint256 price
     ) public payable {
         require(msg.value >= price);
+
+        payable(sellooors[token][id][price]).transfer(msg.value);
 
         buyoooor = msg.sender;
         new BigStepper{salt: keccak256(abi.encode(price))}(token, id);
