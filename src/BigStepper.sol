e16b9e23700c68f57e632ff81a289c4d30b4221b
t11s <greenfeatherhelp@gmail.com>
2022-11-06 19:59:03 -0800

    And both of them off the grid for forgiveness

--- src/BigStepper.sol
+++ src/BigStepper.sol
@@ -5,9 +5,9 @@ import "solmate/tokens/ERC721.sol";
 
 import "./MrMorale.sol";
 
-MrMorale constant morale = MrMorale(address(0xBEEF)); // TODO
-
 contract BigStepper {
+    MrMorale constant morale = MrMorale(address(0xBEEF)); // TODO
+
     constructor(ERC721 token, uint256 id) {
         token.transferFrom(address(this), morale.buyoooor(), id);
     }
---
+++ src/Enoch.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "solmate/tokens/ERC721.sol";
+import "solmate/utils/Bytes32AddressLib.sol";
+
+import "./MrMorale.sol";
+
+contract Enoch {
+    using Bytes32AddressLib for bytes32;
+
+    event MoraleSummoned(MrMorale indexed morale);
+
+    // Reusing MrMorales too frequently runs
+    // the risk of getting them blacklisted.
+    function summonMorale() public {
+        emit MoraleSummoned(new MrMorale());
+    }
+
+    function findStepper(MrMorale morale, ERC721 token, uint256 id, uint256 price, address sellooor)
+        public
+        pure
+        returns (address)
+    {
+        return keccak256(
+            abi.encodePacked(
+                bytes1(0xFF),
+                address(morale),
+                keccak256(abi.encode(price, sellooor)),
+                keccak256(abi.encodePacked(type(BigStepper).creationCode, abi.encode(token, id)))
+            )
+        ).fromLast20Bytes();
+    }
+}
